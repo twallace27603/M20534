a40661351add243b5c15de5c44d00b4dacab5076
twallace27603 <tswwci@hotmail.com>
2016-10-13 13:00:50 -0400

    Update installpayment.sh
    Fixed last line (hopefully)

--- installpayment.sh
+++ installpayment.sh
@@ -4,4 +4,4 @@ cp paysvrstart.sh /etc/init.d
 chmod +x /usr/paysvr/PaymentServer.py
 chmod +x /etc/init.d/paysvrstart.sh
 update-rc.d paysvrstart.sh defaults
-/usr/paysvr/LinuxServer.py
+/usr/paysvr/PaymentServer.py
