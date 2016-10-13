4edf98bff754db699950ea8b08da63aa7713e5f0
twallace27603 <tswwci@hotmail.com>
2016-10-13 11:54:30 -0400

    Update installpayment.sh
    Removed extra had returns from file

--- installpayment.sh
+++ installpayment.sh
@@ -1,15 +1,7 @@
 mkdir /usr/paysvr
-
 cp PaymentServer.py /usr/paysvr
-
 cp paysvrstart.sh /etc/init.d
-
-
-chmod +x /usr/paysvr/LinuxServer.py
-
+chmod +x /usr/paysvr/PaymentServer.py
 chmod +x /etc/init.d/paysvrstart.sh
-
-
 update-rc.d paysvrstart.sh defaults
-
 /usr/paysvr/LinuxServer.py
