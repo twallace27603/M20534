1872fc19c53423e2c61b7eabb41d989a8a4bb269
twallace27603 <tswwci@hotmail.com>
2016-10-13 09:57:22 -0400

    Added installation files for Payment server

---
+++ installpayment.sh
@@ -0,0 +1,15 @@
+mkdir /usr/paysvr
+
+cp PaymentServer.py /usr/paysvr
+
+cp paysvrstart.sh /etc/init.d
+
+
+chmod +x /usr/paysvr/LinuxServer.py
+
+chmod +x /etc/init.d/paysvrstart.sh
+
+
+update-rc.d paysvrstart.sh defaults
+
+/usr/paysvr/LinuxServer.py
---
+++ paysvrstart.sh
@@ -0,0 +1,1 @@
+/usr/paysvr/PaymentServer.py
