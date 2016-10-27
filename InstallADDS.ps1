cc80a7eb1850906990a2b4d6f6def244015414b4
twallace27603 <tswwci@hotmail.com>
2016-10-27 10:49:54 -0400

    Add ADDS PS script

---
+++ InstallADDS.ps1
@@ -0,0 +1,4 @@
+﻿install-windowsfeature AD-domain-services
+Import-Module ADDSDeployment
+$pwd = ConvertTo-SecureString -String "Pa$$w0rd1234" -AsPlainText -Force
+install-ADDSForest -DomainName "testDomain.strikertech.com" -DomainMode 6 -Force -ForestMode 6 -SafeModeAdministratorPassword $pwd 
